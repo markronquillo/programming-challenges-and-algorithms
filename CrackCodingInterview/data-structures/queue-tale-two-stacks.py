4b5ec054e1c0c62554329af7c818277548ec230b
Mark Joseph Ronquillo <markronquillo23@gmail.com>
2017-01-20 14:06:25 +0800

    Add queue and trees

--- CrackCodingInterview/data-structures/queue-tale-two-stacks.py
+++ CrackCodingInterview/data-structures/queue-tale-two-stacks.py
@@ -14,6 +14,9 @@ class Stack(object):
         else:
             return val
 
+    def empty(self):
+        return len(self.storage) == 0
+
     def tos(self):
         if len(self.storage):
             return self.storage[len(self.storage)-1]
@@ -24,25 +27,26 @@ class Stack(object):
 class MyQueue(object):
 
     def __init__(self):
-        self.stack1 = Stack()
-        self.stack2 = Stack()
+        self.enqueue = Stack()
+        self.dequeue = Stack()
 
     def peek(self):
-        self._move(self.stack1, self.stack2)
-        return self.stack2.tos()
+        if self.dequeue.empty():
+            self._move(self.enqueue, self.dequeue)
+        return self.dequeue.tos()
 
     def pop(self):
-        self._move(self.stack1, self.stack2)
-        self.stack2.pop()
+        if self.dequeue.empty():
+            self._move(self.enqueue, self.dequeue)
+        return self.dequeue.pop()
 
     def put(self, value):
-        self._move(self.stack2, self.stack1)
-        self.stack1.push(value)
+        self.enqueue.push(value)
 
     def _move(self, stack_from, stack_to):
         while True:
             val = stack_from.pop()
-            if val is None: 
+            if val is None:
                 break
             stack_to.push(val)
 
@@ -52,8 +56,8 @@ for line in range(t):
     values = map(int, input().split())
     values = list(values)
     if values[0] == 1:
-        queue.put(values[1])        
+        queue.put(values[1])
     elif values[0] == 2:
         queue.pop()
     else:
-        print(queue.peek()) 
+        print(queue.peek())
--- CrackCodingInterview/data-structures/trees-is-this-binary-search-tree.py
+++ CrackCodingInterview/data-structures/trees-is-this-binary-search-tree.py
@@ -15,8 +15,26 @@ def check_binary_search_tree(root):
             traverse(current.right, l)
 
     traverse(root, lst)
-    print lst
+
+    if len(set(lst)) <> len(lst):
+        return False
+
+    if sorted(lst) == lst:
+        return True
+    else:
+        return False
 
 
+head = node(10)
+left = node(5)
+right = node(14)
+
+head.left = left
+head.right = right
+
+random = node(6)
+left.right = random
+
+print(check_binary_search_tree(head))
 
 
